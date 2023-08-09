82af3793463d66ccd94f6b7db2a76234fd31df95
hermescnp <39063620+hermescnp@users.noreply.github.com>
2023-08-09 19:44:23 -0400

    update-nextjs-connfig
    images: { unoptimized: true }

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' }
+const nextConfig = { 
+    output: 'export',
+    images: { unoptimized: true }
+ }
 
 module.exports = nextConfig
