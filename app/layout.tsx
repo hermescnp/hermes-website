659370034217858535d1eae78282128d252f7772
hermescnp <39063620+hermescnp@users.noreply.github.com>
2023-10-18 19:15:38 -0400

    responsive_adjustments
    chatbot responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import './globals.css'
 import { ExperienceProvider } from '@/context/ExperienceContext';
 import App from './App';
@@ -10,6 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ExperienceProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
       <App>{children}</App>
     </ExperienceProvider>
   );
