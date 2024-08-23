bcf671b7ac51d9fda8c8b7f9a541558ab6ef3ed6
hermescnp <39063620+hermescnp@users.noreply.github.com>
2024-08-23 12:15:28 -0400

    maxDistance-test
    testing if maxDistance is available for use

--- components/Experience/Controls.tsx
+++ components/Experience/Controls.tsx
@@ -13,12 +13,12 @@ export default function Controls(camera : any, target : any, renderer : any, dat
     controls.rotateSpeed = 0.40;
     controls.enablePan = false;
     controls.target = target?.position;
-    controls.maxDistance = data[0].maxDistance;
-    controls.minDistance = data[0].mixDistance;
-    controls.maxAzimuthAngle = data[0].maxAzimuthAngle;
-    controls.minAzimuthAngle = data[0].minAzimuthAngle;
-    controls.maxPolarAngle = Math.PI/data[0].maxPolarAngle;
-    controls.minPolarAngle = Math.PI/data[0].minPolarAngle;
+    controls.maxDistance = data[0]?.maxDistance;
+    controls.minDistance = data[0]?.mixDistance;
+    controls.maxAzimuthAngle = data[0]?.maxAzimuthAngle;
+    controls.minAzimuthAngle = data[0]?.minAzimuthAngle;
+    controls.maxPolarAngle = Math.PI/data[0]?.maxPolarAngle;
+    controls.minPolarAngle = Math.PI/data[0]?.minPolarAngle;
     controls.autoRotate = true;
     controls.autoRotateSpeed = 0.2;
 
