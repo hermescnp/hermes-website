910f2f93562b758aa4481ae3f9f762822985ad3d
hermescnp <hermes.cnp@gmail.com>
2025-01-08 00:57:36 -0400

    recreate_render_style
    recreate the renderer settings, ambient, background and reflections style to metch the look from the plain ThreeJS to the new R3F version

--- components/Canvas/Canvas.tsx
+++ components/Canvas/Canvas.tsx
@@ -1,16 +1,38 @@
 "use client"
-import React, { useRef, MutableRefObject } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React from 'react'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import SceneModel from './SceneModel'
+import Camera from '../Experience/Camera_R3F'
+import * as THREE from 'three';
+import Background_R3F from '../Experience/Background_R3F';
 
 const CanvasComponent = () => {
+    const position = new THREE.Vector3(0, 0, 5);
+    const target = new THREE.Vector3(0, 0, 0);
+    const aspect = window.innerWidth / window.innerHeight;
 
     return (
-        <Canvas style={{ width: '100vw', height: '100vh' }}>
+        <Canvas
+            style={{ width: '100vw', height: '100vh' }}
+            gl={{
+                antialias: true,
+                toneMapping: THREE.LinearToneMapping,
+                toneMappingExposure: 1.0,
+                shadowMapEnabled: false,
+                shadowMapType: THREE.PCFSoftShadowMap,
+                preserveDrawingBuffer: false
+            }}
+            onCreated={({ scene }) => {
+                const color = new THREE.Color(0x1e2332);
+                scene.fog = new THREE.Fog(color, 50, 70);
+                scene.background = null;
+            }}
+        >
+            <ambientLight color="#ffe175" intensity={0.2} />
+            <Background_R3F />
+            <Camera position={position} target={target} aspect={aspect} />
             <OrbitControls />
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
             <SceneModel />
         </Canvas>
     );
---
+++ components/Experience/Camera_R3F.tsx
@@ -0,0 +1,27 @@
+import { useThree } from '@react-three/fiber';
+import { useEffect } from 'react';
+import * as THREE from 'three';
+
+export default function Camera({ position, target, aspect }: { position: THREE.Vector3, target: THREE.Vector3, aspect: number }) {
+  const { set, camera } = useThree();
+
+  useEffect(() => {
+    const perspectiveCamera = new THREE.PerspectiveCamera(
+      25,
+      aspect,
+      0.01,
+      1000
+    );
+
+    perspectiveCamera.position.copy(position);
+    perspectiveCamera.lookAt(target);
+
+    set({ camera: perspectiveCamera });
+
+    return () => {
+      // Clean up if necessary
+    };
+  }, [position, target, aspect, set]);
+
+  return null;
+}
